d0460e0c975a2addc104c080e1e8846667fb5541
MrYapikZ <yafialhaidar18@gmail.com>
2026-03-03 10:20:11 +0700

    style: improve login form text visibility

--- app/ui/startup/login_ui.py
+++ app/ui/startup/login_ui.py
@@ -13,7 +13,8 @@ class Ui_Form(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(280, 277)
-        Form.setStyleSheet("background: white;")
+        Form.setStyleSheet("background: white;\n"
+"color: black;")
         self.gridLayout = QtWidgets.QGridLayout(Form)
         self.gridLayout.setObjectName("gridLayout")
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Policy.Minimum, QtWidgets.QSizePolicy.Policy.Expanding)
